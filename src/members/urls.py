caa7225ee111c9556411987249e5bb2e7252d593
meltem <meltemkaraagac19@gmail.com>
2020-12-31 16:11:28 -0500

    add

---
+++ src/members/urls.py
@@ -0,0 +1,10 @@
+
+from django.urls import path
+from .views import UserRegisterView
+
+
+urlpatterns = [
+    path('register/', UserRegisterView.as_view(), name='register')
+
+
+]
---
+++ src/members/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.views import generic
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django.urls import reverse_lazy
+
+# Create your views here.
+
+
+class UserRegisterView(generic.CreateView):
+    form_class = UserCreationForm
+    template_name = 'registration/register.html'
+    success_url = reverse_lazy('login')
