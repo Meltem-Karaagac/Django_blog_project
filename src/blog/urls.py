cc093f418c6cc4517855f1da9bb8c74e75daa34a
meltem <meltemkaraagac19@gmail.com>
2021-01-05 14:58:32 -0500

    add

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import post_list, post_create
+from .views import post_list, post_create, post_detail, post_update, post_delete
 
 
 app_name = "blog"
 urlpatterns = [
     path("", post_list, name="list"),
-    path("create/", post_create, name="create")
+    path("create/", post_create, name="create"),
+    path("<str:slug>/", post_detail, name="detail"),
+    path("<str:slug>/update/", post_update, name="update"),
+    path("<str:slug>/delete/", post_delete, name="delete"),
 ]
--- src/blog/views.py
+++ src/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Post
 from .forms import PostForm
 
@@ -26,3 +26,37 @@ def post_create(request):
         'form': form
     }
     return render(request, "blog/post_create.html", context)
+
+
+def post_detail(request, slug):
+    # Post.objects.get(slug=learn-drf-3c78be2186)
+    obj = get_object_or_404(Post, slug=slug)  # slug = learn-drf-3c78be2186
+    context = {
+        "object": obj
+    }
+    return render(request, "blog/post_detail.html", context)
+
+
+def post_update(request, slug):
+    obj = get_object_or_404(Post, slug=slug)
+    form = PostForm(request.POST or None, request.FILES or None, instance=obj)
+    if form.is_valid():
+        form.save()
+        return redirect("blog:list")
+
+    context = {
+        "object": obj,
+        "form": form
+    }
+    return render(request, "blog/post_update.html", context)
+
+
+def post_delete(request, slug):
+    obj = get_object_or_404(Post, slug=slug)
+    if request.method == "POST":
+        obj.delete()
+        return redirect("blog:list")
+    context = {
+        "object": obj
+    }
+    return render(request, "blog/post_delete.html", context)
